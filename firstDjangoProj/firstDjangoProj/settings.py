a3c14fa7f16a59231771cd44ced4ff9952cba3f6
robindasraam <robindasraam@gmail.com>
2024-03-19 14:36:08 +0600

    added static files

--- firstDjangoProj/firstDjangoProj/settings.py
+++ firstDjangoProj/firstDjangoProj/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
+STATIC_DIR = os.path.join(BASE_DIR,"static")
 
 # Application definition
 
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [STATIC_DIR,]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
